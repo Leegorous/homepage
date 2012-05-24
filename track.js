728d7f866d80d68d242178c2254defcaca1e631d
Leegorous <leegorous@gmail.com>
2012-05-24 14:08:25 +0800

    exclude tracking page view from local network

--- track.js
+++ track.js
@@ -2,7 +2,7 @@ var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-424296-1']);
 _gaq.push(['_trackPageview']);
 
-if (location.hostname != 'localhost') (function() {
+if (!/^(localhost|\d{1,3}(\.\d{1,3}){3})$/.test(location.hostname)) (function() {
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
