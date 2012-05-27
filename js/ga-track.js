b4567baaaad4b52cc1fc995c0e76303316d77be6
Leegorous <leegorous@gmail.com>
2012-05-27 11:19:32 +0800

    refactor & add minified version

--- js/ga-track.js
+++ js/ga-track.js
@@ -1,9 +1,18 @@
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-424296-1']);
-_gaq.push(['_trackPageview']);
+!function(doc, disabled) {
+    var _gaq = (window._gaq = window._gaq || [])
+    _gaq.push(['_setAccount', 'UA-424296-1'])
+    _gaq.push(['_trackPageview'])
 
-if (!/^(localhost|\d{1,3}(\.\d{1,3}){3})$/.test(location.hostname)) (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+    if (!disabled) {
+
+        var ga = doc.createElement('script')
+          , s = doc.getElementsByTagName('script')[0]
+
+        ga.type = 'text/javascript'
+        ga.async = true
+        ga.src = ('https:' == doc.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'
+        s.parentNode.insertBefore(ga, s)
+
+    }
+
+}(document, /^(localhost|\d{1,3}(\.\d{1,3}){3})$/.test(location.hostname));
